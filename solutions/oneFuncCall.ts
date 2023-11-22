97333e9f3cc8b662f3c4c27ced592cfbc4c9b5f4
wassimxd <karimmohsin10@gmail.com>
2023-11-22 16:41:40 +0100

    Allow One Function Call

---
+++ solutions/oneFuncCall.ts
@@ -0,0 +1,32 @@
+/* 
+2666. Allow One Function Call 
+Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+*/
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+function once(fn: Function): OnceFn {
+  let hasBeenCalled = false;
+  return function (...args) {
+    const result = fn(...args);
+    if (!hasBeenCalled) {
+      hasBeenCalled = true;
+      return result;
+    }
+  };
+}
+
+let fn = (a, b, c) => a + b + c;
+let onceFn = once(fn);
+
+console.log(onceFn(1, 2, 3)); // 6
+console.log(onceFn(2, 3, 6)); // returns undefined without calling fn
